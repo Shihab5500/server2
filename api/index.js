3ac9a84a248931d52e9a6e29f7c761748b56c3c6
Shihab5500 <Shihabahmed6501@gmail.com>
2025-12-07 16:35:31 +0600

    change

--- api/index.js
+++ api/index.js
@@ -1,7 +1,5 @@
-// api/index.js (Vercel Entry Point)
 
-// আপনার মূল Express অ্যাপ ফাইলটিকে Import করুন
 import app from "../src/index.js";
 
-// Vercel-এর জন্য Express অ্যাপটিকে export করুন
+
 export default app;
